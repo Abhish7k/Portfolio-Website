ba94d2009258f4ebbb9b247178577ad9ba4ecf70
Abhishek <abhishekbhosale.dev@gmail.com>
2023-10-05 13:31:04 +0530

    initial commit

---
+++ components/about.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+
+      <p className="mb-3">
+        As a dedicated{" "}
+        <span className="font-bold">Full-stack developer</span> with 
+        with a strong foundation in both frontend and backend technologies,
+        I specialize in a stack that includes{" "}
+        <span className="font-medium">Javascript, React and Next.js</span>{" "}
+        on the frontend.
+        In addition to my frontend expertise, I'm also well-versed in backend technologies like{" "}
+        <span className="font-medium" >
+          Node.js, Express.js
+        </span>{" "}
+        and databases such as{" "}
+        <span className="font-medium" >
+          PostgreSQL and MongoDB. 
+        </span>{" "}
+        My proficiency extends to 
+        TypeScript and Java, 
+        enabling me to craft versatile software solutions that encompass the entire development stack.
+        <br />
+        <br />
+        <br />
+        I'm a final year computer science student expected to graduate in July
+        2024. Proficient in data structures and algorithms, and fueled by a passion for problem solving, I regularly
+        solve problems on platforms like <span className="font-medium"><a href="https://leetcode.com/Abhish7k/" target="_blank" >LeetCode</a></span>{" "}
+        and <span className="font-medium"><a href="https://auth.geeksforgeeks.org/user/abhish3k/" target="_blank">GeeksforGeeks</a></span> using{" "}
+        <span className="font-medium">C++.</span>
+      </p>
+      <br />  
+
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,110 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import NextpixImg from "@/public/Nextpix.png";
+import PokedexImg from "@/public/Pokedex.png";
+import TodoAppImg from "@/public/TodoApp.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Education",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "B.E. Computer Science",
+    location: "Savitribai Phule Pune University, Pune, IN",
+    description:
+      "CGPA: 7.0, Coursework: Object Oriented Programming, Database Management System, Computer Networks, Operating Systems.",
+    icon: React.createElement(LuGraduationCap),
+    date: "Aug 2020 - July 2024",
+  },
+  {
+    title: "High School",
+    location: "Modern Education Society's Nowrosjee Wadia College, Pune, IN",
+    description:
+      "HSC Science",
+    icon: React.createElement(LuGraduationCap),
+    date: "2018 - 2020",
+  },
+  // {
+  //   title: "Full-Stack Developer",
+  //   location: "",
+  //   description:
+  //     "I'm now a full-stack developer.",
+  //   icon: React.createElement(FaReact),
+  //   date: "2023 - present",
+  // },
+] as const;
+
+export const projectsData = [
+  {
+    title: "NextPix",
+    description:
+      "Image Gallery app built using Next.js.",
+    tags: ["TypeScript", "React", "Next.js", "Tailwind"],
+    imageUrl: NextpixImg,
+  },
+  {
+    title: "Pokédex",
+    description:
+      "A Pokédex app powered by Next.js. Effortlessly search and discover over 800 Pokémon species with their detailed information.",
+    tags: ["TypeScript", "React", "Next.js", "Tailwind"],
+    imageUrl: PokedexImg,
+  },
+  {
+    title: "Todo App",
+    description:
+      "A Modern Todo App build using React.",
+    tags: ["Typescript", "React", "Tailwind"],
+    imageUrl: TodoAppImg,
+  },
+] as const;
+
+export const skillsData = [
+  "C++",
+  "Java",
+  "JavaScript",
+  "TypeScript",
+  "Data Structures",
+  "Algorithms",
+  "HTML",
+  "CSS",
+  "Tailwind",
+  "React",
+  "Redux",
+  "Next.js",
+  "Node.js",
+  "Express.js",
+  "REST Api",
+  "PostgreSQL",
+  "MongoDB",
+  "Prisma",
+  "Redis",
+  "Git",
+  "GitHub",
+  "Docker",
+] as const;
